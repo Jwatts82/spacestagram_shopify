634eb70da30b62ed05ebbaf1e2435e42efc4036f
Jwatts82 <wattscodes@gmail.com>
2022-01-18 20:20:01 -0800

    fetch data from nasa

--- src/containers/Posts.js
+++ src/containers/Posts.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 
+const My_Key = process.env.REACT_APP_NASA_API_KEY;
+
 class Posts extends Component {
+    state={
+        posts: []
+    }
+
+    componentDidMount(){
+        fetch(
+          `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${My_Key}`)
+          .then(res => res.json())
+          .then(data => {
+              console.log(data.photos)
+          })
+    }
+
   render() {
     return  (
         <div>
