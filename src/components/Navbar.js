b3dea66b6bc6e9ee9bba5b5757e8b52c30d74387
Jwatts82 <wattscodes@gmail.com>
2022-01-18 21:47:38 -0800

    eliminate uneeded nav items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,9 +7,6 @@ function Navbar() {
       <nav className='navbar'>
         <div className='nav-wrapper'>
           <h2 type='text'>Spacestagram</h2>
-          <div className='nav-items'>
-            <div className='icon user-profile'></div>
-          </div>
         </div>
       </nav>
     </div>
