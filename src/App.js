d1fd70771e67987d2e84f90e9bb9c885732f5452
Jwatts82 <wattscodes@gmail.com>
2022-01-18 20:11:25 -0800

    create Post container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Posts from './containers/Posts'
 function App() {
   return (
     <div className="App">
      <h1>Spacestagram</h1>
+        <Posts />
     </div>
   );
 }
