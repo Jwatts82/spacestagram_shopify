a68bf9edfe09578f9d6b1152b189a02151ef3bce
Jwatts82 <wattscodes@gmail.com>
2022-01-19 12:37:38 -0800

    add status card

--- src/components/Status.js
+++ src/components/Status.js
@@ -4,39 +4,45 @@ import '../styles/Status.css'
 function Status() {
   return (
     <div>
-      <section class='main'>
-        <div class='wrapper'>
-          <div class='left-col'>
-            <div class='status-wrapper'>
-              <div class='status-card'>
-                <div class='profile-pic'>
+      <section className='main'>
+        <div className='wrapper'>
+          <div className='left-col'>
+            <div className='status-wrapper'>
+              <div className='status-card'>
+                <div className='profile-pic'>
                   <img src='cover 1.jpg' alt='img' />
                 </div>
-                <p class='username'>Jared Rice</p>
+                <p className='username'>Jared Rice</p>
               </div>
-              <div class='status-card'>
-                <div class='profile-pic'>
+              <div className='status-card'>
+                <div className='profile-pic'>
                   <img src='cover 2.jpg' alt='img' />
                 </div>
-                <p class='username'>A_Odie</p>
+                <p className='username'>A_Odie</p>
               </div>
-              <div class='status-card'>
-                <div class='profile-pic'>
+              <div className='status-card'>
+                <div className='profile-pic'>
                   <img src='cover 3.jpg' alt='img' />
                 </div>
-                <p class='username'>Meesh</p>
+                <p className='username'>Meesh</p>
               </div>
-              <div class='status-card'>
-                <div class='profile-pic'>
+              <div className='status-card'>
+                <div className='profile-pic'>
                   <img src='cover 4.jpg' alt='img' />
                 </div>
-                <p class='username'>Watts_Happen</p>
+                <p className='username'>Watts_Happen</p>
               </div>
-              <div class='status-card'>
-                <div class='profile-pic'>
+              <div className='status-card'>
+                <div className='profile-pic'>
                   <img src='cover 5.jpg' alt='img' />
                 </div>
-                <p class='username'>Spc_Frntr</p>
+                <p className='username'>Spc_Frntr</p>
+              </div>
+              <div className='status-card'>
+                <div className='profile-pic'>
+                  <img src='cover 6.jpg' alt='img' />
+                </div>
+                <p className='username'>Gideon</p>
               </div>
             </div>
           </div>
